c744789076018b940244e2ba3e9005d926a31ece
xiashan <xiashan@gmail.com>
2017-07-08 15:53:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tianyi/mybroad2/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.tianyi.mybroad2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private IntentFilter intentFilter,intentFilter1;
+    private LocalBroadcastManager localBroadcastManager;
+    private LocalReceiver localReceiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        Button button = (Button) findViewById(R.id.send_broad);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
+//                sendBroadcast(intent);
+                Intent intent = new Intent("com.example.tianyi.mybroad");
+                localBroadcastManager.sendBroadcast(intent);
+            }
+        });
+        intentFilter = new IntentFilter();
+        intentFilter1 = new IntentFilter();
+        intentFilter.addAction("com.example.tianyi.mybroad");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver,intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        localBroadcastManager.unregisterReceiver(localReceiver);
+    }
+
+    class LocalReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context,"Hey Body",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tianyi/mybroad2/MyBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package com.example.tianyi.mybroad2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"这是另一个广播",Toast.LENGTH_SHORT).show();
+    }
+}
